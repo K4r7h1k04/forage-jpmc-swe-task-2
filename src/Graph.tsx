b2f1cf14e3a985a24f21cc1cd5d5e7c134d1ae08
karthik <124885785+K4r7h1k04@users.noreply.github.com>
2023-07-02 17:27:41 +0530

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,25 +14,26 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
 /**
  * React component that renders Perspective based on data
- * parsed from its parent through data property.
+ * parsed from its parent through the data property.
  */
 class Graph extends Component<IProps, {}> {
   // Perspective table
   table: Table | undefined;
 
   render() {
+    // Render an empty <perspective-viewer> element
     return React.createElement('perspective-viewer');
   }
 
   componentDidMount() {
-    // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    // Get the <perspective-viewer> element from the DOM
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -41,22 +42,30 @@ class Graph extends Component<IProps, {}> {
       timestamp: 'date',
     };
 
-    if (window.perspective && window.perspective.worker()) {
+    // Check if the Perspective library is available
+    if (window.perspective) {
+      // Create a Perspective table using the schema
       this.table = window.perspective.worker().table(schema);
     }
+
     if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
+      // Load the `table` into the `<perspective-viewer>` element
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('aggregates', '{"stock":"distinct count","top_ask_price":"avg","top_bid_price":"avg","timestamp":"distinct count"}');
     }
   }
 
   componentDidUpdate() {
-    // Everytime the data props is updated, insert the data into Perspective table
+    // Every time the data prop is updated, insert the data into the Perspective table
     if (this.table) {
-      // As part of the task, you need to fix the way we update the data props to
-      // avoid inserting duplicated entries into Perspective table again.
+      // As part of the task, you need to fix the way we update the data prop to
+      // avoid inserting duplicate entries into the Perspective table again.
       this.table.update(this.props.data.map((el: any) => {
         // Format the data from ServerRespond to the schema
         return {
